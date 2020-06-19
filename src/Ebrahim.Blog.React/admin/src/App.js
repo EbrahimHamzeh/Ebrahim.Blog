8d2d71438e8bf78ba5e5a5a8be827adbd37d3b34
Ebrahim <pranc14@gmail.com>
2020-06-19 15:18:42 +0430

    add panel layout to react

--- src/Ebrahim.Blog.React/admin/src/App.js
+++ src/Ebrahim.Blog.React/admin/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import Login from './components/identity/layout/Login';
+import Login from './components/layout/Login';
+import Panel from './components/layout/Panel';
 
 function App() {
   return (
-    <Login></Login>
+    <Panel></Panel>
   );
 }
 
